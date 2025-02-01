6296ec2372b40b25c238f5ee9cbf248d63ab3482
Mate Kavelashvili <mateyavelashvili@gmail.com>
2025-02-01 19:15:55 +0400

    19:15 Created Palindrome Checker

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter your word");
+        string input = Console.ReadLine();
+        Console.WriteLine(sPalindrome(input));
+    }
+
+    static bool sPalindrome(string text)
+    {
+        if (string.IsNullOrEmpty(text)) return false;
+        
+            char[] splittedInput = text.ToCharArray();
+            Array.Reverse(splittedInput);
+            string reversedString = new string(splittedInput);
+            return text.Equals(reversedString);
+    }
+}
