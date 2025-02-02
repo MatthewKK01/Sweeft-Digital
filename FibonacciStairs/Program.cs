c6e0a3833ab2be995026f4ea3bb016c9342ec840
Mate Kavelashvili <mateyavelashvili@gmail.com>
2025-02-02 11:58:06 +0400

    Added Fibonacci and Isproperly check

---
+++ FibonacciStairs/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+   private static int CountVariants(int stairCount)
+    {
+        if (stairCount == 1) return 1;
+        if (stairCount == 2) return 2;
+
+        var prev1 = 2; // f(n-1)
+        var prev2 = 1; // f(n-2)
+        var current = 0;
+
+        for (var i = 3; i <= stairCount; i++)
+        {
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+
+   public static void Main()
+    {
+        Console.Write("Enter the number of steps: ");
+        var n = int.Parse(Console.ReadLine());
+
+        var result = CountVariants(n);
+        Console.WriteLine(result);
+    }
+}
---
+++ IsProperly/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+{
+   private  static bool IsProperly(string sequence)
+    {
+        var count = 0;
+
+        foreach (var c in sequence)
+        {
+            if (c == '(')
+            {
+                count++;
+            }
+            else if (c == ')')
+            {
+                count--;
+                if (count < 0) return false;
+            }
+        }
+
+        return count == 0; 
+    }
+
+    public static void Main()
+    {
+        
+        Console.WriteLine(IsProperly("(()())")); // true
+        Console.WriteLine(IsProperly("())()"));  // false
+        Console.WriteLine(IsProperly("((()))")); // true
+        Console.WriteLine(IsProperly(")("));     // false
+    }
+}
