3767e77f393500f81bef5c826463da665522cdcb
MatthewKK01 <mateyavelashvili@gmail.com>
2025-02-03 10:02:00 +0400

    created MatrixApp

---
+++ MatrixPrint/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1, 1);
+    private static bool running = true;
+
+    static async Task Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Task printTask = PrintNumbersAsync();
+        Task messageTask = ShowMessagePeriodicallyAsync();
+
+        await Task.WhenAll(printTask, messageTask);
+    }
+
+    static async Task PrintNumbersAsync()
+    {
+        while (running)
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write("10");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+            await Task.Delay(10);
+        }
+    }
+
+    static async Task ShowMessagePeriodicallyAsync()
+    {
+        while (running)
+        {
+            await Task.Delay(5000);
+            await semaphore.WaitAsync();
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nNeo, you are the chosen one");
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+            await Task.Delay(5000);
+        }
+    }
+}
